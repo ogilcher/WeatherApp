e59350de6183bb2305c4634daf0b3da3f701590d
ogilcher <olivergilcher@gmail.com>
2025-02-12 16:37:08 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Oliver Gilcher on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
